b9971ba15cd25e185c3164399b9d817eb54c98e6
Hari <harimurali10a@gmail.com>
2026-08-21 11:31:42 +0530

    Fix: remove unused imports in Chatbot.jsx to pass ESLint CI build

--- frontend/src/components/Chatbot/Chatbot.jsx
+++ frontend/src/components/Chatbot/Chatbot.jsx
@@ -1,25 +1,8 @@
-import React, { useEffect, useState } from 'react';
-import voiceflowConfig from '../../config/voiceflow';
-
-const SparklesIcon = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-    <path d="m12 3-1.912 5.813a2 2 0 0 1-1.275 1.275L3 12l5.813 1.912a2 2 0 0 1 1.275 1.275L12 21l1.912-5.813a2 2 0 0 1 1.275-1.275L21 12l-5.813-1.912a2 2 0 0 1-1.275-1.275L12 3Z" />
-    <path d="M5 3v4" />
-    <path d="M19 17v4" />
-    <path d="M3 5h4" />
-    <path d="M17 19h4" />
-  </svg>
-);
-
-const CloseIcon = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-    <line x1="18" y1="6" x2="6" y2="18" />
-    <line x1="6" y1="6" x2="18" y2="18" />
-  </svg>
-);
+import React from 'react';
 
 const Chatbot = () => {
   return null;
 };
 
 export default Chatbot;
+
